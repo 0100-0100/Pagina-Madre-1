2df51d145eb3aed5fefda061149eeb2f4dcea9bd
Diego <dfelo93@gmail.com>
2026-01-19 09:17:33 -0500

    feat(02-01): configure admin interface for CustomUser
    - Create CustomUserAdmin extending UserAdmin
    - Add custom fields to list_display (cedula, phone, data_policy_accepted)
    - Add custom fields to fieldsets for detail view
    - Add custom fields to add_fieldsets for user creation
    - Register CustomUser with CustomUserAdmin

--- ___/accounts/admin.py
+++ ___/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    # Display custom fields in admin list view
+    list_display = UserAdmin.list_display + ('cedula', 'phone', 'data_policy_accepted')
+
+    # Add custom fields to admin detail view
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('cedula', 'nombre_completo', 'phone', 'data_policy_accepted')}),
+    )
+
+    # Include custom fields when adding new user via admin
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('cedula', 'nombre_completo', 'phone', 'data_policy_accepted')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
