2c982b726c63e96874803490c08a382548cf17bb
Diego <dfelo93@gmail.com>
2026-01-19 15:11:33 -0500

    feat(07-01): update admin to display referral fields
    - Add referral_code, referred_by, referral_goal to list_display
    - Add Referral Info section in detail view
    - Make referral_code read-only

--- ___/accounts/admin.py
+++ ___/accounts/admin.py
@@ -5,16 +5,28 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     # Display custom fields in admin list view
-    list_display = UserAdmin.list_display + ('cedula', 'phone', 'data_policy_accepted')
+    list_display = UserAdmin.list_display + (
+        'cedula', 'phone', 'referral_code', 'referred_by', 'referral_goal'
+    )
+
+    # Make referral_code read-only (auto-generated)
+    readonly_fields = ('referral_code',)
 
     # Add custom fields to admin detail view
     fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('cedula', 'nombre_completo', 'phone', 'data_policy_accepted')}),
+        ('Additional Info', {
+            'fields': ('cedula', 'nombre_completo', 'phone', 'data_policy_accepted')
+        }),
+        ('Referral Info', {
+            'fields': ('referral_code', 'referred_by', 'referral_goal')
+        }),
     )
 
     # Include custom fields when adding new user via admin
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Additional Info', {'fields': ('cedula', 'nombre_completo', 'phone', 'data_policy_accepted')}),
+        ('Additional Info', {
+            'fields': ('cedula', 'nombre_completo', 'phone', 'data_policy_accepted')
+        }),
     )
 
 
